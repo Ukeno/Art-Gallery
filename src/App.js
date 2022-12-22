56b9cab725b1264e63173ebec12068b31f7762e2
Kenny K <koablood@gmail.com>
2022-12-21 21:27:19 -0800

    123

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import ButtonBar from './Components/ButtonBar'
+import Gallery from './Components/Gallery'
+import {useState, useEffect} from 'react'
+
+function App() {
+  let [artId, setArtId] = useState(Math.floor(Math.random() * (471581 - 1 + 1)) + 1)
+  let [data, setData] = useState({})
+  
+  useEffect(() => {
+    document.title = 'Welcome to ArtWorld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+  }, [artId])
+
+  const handleIterate = e => {
+    if(artId + Number(e.target.value) > 471581) {
+      setArtId(471581)
+    } else if (artId + Number(e.target.value) < 1) {
+      setArtId(1)
+    } else {
+      setArtId(artId + Number(e.target.value))
+    }
+  }
+  
+  return (
+    <div className="App">
+      <Gallery primaryImage={data.primaryImage} title={data.title} artist={data.artistDisplayName}
+        medium={data.medium} />
+      <ButtonBar handleIterate={handleIterate} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ButtonBar.jsx
@@ -0,0 +1,13 @@
+function ButtonBar(props) {
+    return (
+      <div className="buttons">
+        <button value={-5} onClick={props.handleIterate}>Way Back</button>
+        <button value={-1} onClick={props.handleIterate}>Back</button>
+        <button value={1} onClick={props.handleIterate}>Next</button>
+        <button value={100} onClick={props.handleIterate}>HUGE Next</button>
+        <button value={Math.floor(Math.random() * (471581 - 1 + 1)) + 1} onClick={props.handleIterate}>Pick for me!</button>
+      </div>
+    )
+  }
+  
+  export default ButtonBar
